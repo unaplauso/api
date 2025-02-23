04208d028a2552581fcd31d9d60b9e44fdb2b628
Luka Cerrutti <lukacerrutti2002@gmail.com>
2025-02-23 20:03:46 -0300

    composite test
    Signed-off-by: Luka Cerrutti <lukacerrutti2002@gmail.com>

--- apps/gateway/src/controllers/health.controller.ts
+++ apps/gateway/src/controllers/health.controller.ts
@@ -4,8 +4,9 @@ import { InjectDB } from '@unaplauso/database';
 import { InjectCLI, InternalService, Service } from '@unaplauso/services';
 import { serviceExists } from '@unaplauso/services/utils';
 import { NodePgDatabase } from 'drizzle-orm/node-postgres';
+import { SuperProtected } from '../decorators/super-protected.decorator';
 
-// @SuperProtected()
+@SuperProtected()
 @Controller('health')
 export class HealthController {
   constructor(
