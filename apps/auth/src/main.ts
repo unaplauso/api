4149b0720f465d9ee46577677a338e6e974f215f
Luka Cerrutti <lukacerrutti2002@gmail.com>
2025-03-04 19:18:32 -0300

    proxy trust on auth
    Signed-off-by: Luka Cerrutti <lukacerrutti2002@gmail.com>

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import session from 'express-session';
 import { AuthModule } from './auth.module';
+import { IS_DEVELOPMENT } from '@unaplauso/common/validation';
 
 (async () => {
-  const app = await NestFactory.create(AuthModule);
+  const app = await NestFactory.create<NestExpressApplication>(AuthModule);
   app.setGlobalPrefix('api/auth');
+
+  if (!IS_DEVELOPMENT) app.set('trust proxy', 1);
 
   app.use(
     session({
